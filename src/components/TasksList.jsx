64e8e429ce116fca4315f3814b69630fa31db0c6
Jose Rafael Guaura <jguaura@gmail.com>
2023-03-20 19:54:01 -0500

    feat:
    - DELETE mutation

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useGetTasksQuery } from '../api/apiSlice';
+import { useGetTasksQuery, useDeleteTaskMutation } from '../api/apiSlice';
 
 function TasksList() {
   const { data: tasks, isError, isLoading, error } = useGetTasksQuery();
+  const [deleteTaks] = useDeleteTaskMutation()
 
   if (isLoading) return <div>Loading...</div>
   else if (isError) return <div>Error: {error.message}</div>;
@@ -13,7 +14,7 @@ function TasksList() {
         <li key={task.id}>
           <h3>{task.name}</h3>
           <p>{task.description}</p>
-          <button>delete</button>
+          <button onClick={() => deleteTaks(task.id)}>delete</button>
           <input type="checkbox" id={task.id} />
           <label htmlFor={task.id}>completed</label>
         </li>
