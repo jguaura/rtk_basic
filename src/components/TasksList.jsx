db53f90e24a54267471221557ab59d9f235a1d05
Jose Rafael Guaura <jguaura@gmail.com>
2023-03-20 19:31:41 -0500

    feat:
    - api slice + queries

---
+++ src/components/TasksList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useGetTasksQuery } from '../api/apiSlice';
+
+function TasksList() {
+  const { data: tasks, isError, isLoading, error } = useGetTasksQuery();
+
+  if (isLoading) return <div>Loading...</div>
+  else if (isError) return <div>Error: {error.message}</div>;
+
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <h3>{task.name}</h3>
+          <p>{task.description}</p>
+          <button>delete</button>
+          <input type="checkbox" id={task.id} />
+          <label htmlFor={task.id}>completed</label>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default TasksList;
